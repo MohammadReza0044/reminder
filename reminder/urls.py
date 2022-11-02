86fcbad27a3b8612c4c6b90989287ef221204332
MohammadReza0044 <you@example.com>
2022-11-02 16:25:18 +0330

    almost done

--- reminder/urls.py
+++ reminder/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path 
-from .views import reminder_list, reminder_detail, delete, rimender_Update, reminder_total_list
+from .views import reminder_list, reminder_detail, reminder_Delete, rimender_Update, reminder_total_list
 
 urlpatterns = [
     path('', reminder_list.as_view(), name='reminder_list'),
     path('reminders', reminder_total_list.as_view(), name='reminder_total'),
-    path('<pk>', reminder_detail.as_view(), name='reminder_detail' ),
-    path('/delete/<pk>', delete, name='reminder_delete' ),
-    path('/update/<pk>', rimender_Update.as_view(), name='reminder_update' ),
+    path('<int:pk>', reminder_detail.as_view(), name='reminder_detail' ),
+    path('/delete/<int:pk>', reminder_Delete.as_view(), name='reminder_delete' ),
+    path('/update/<int:pk>', rimender_Update.as_view(), name='reminder_update' ),
     
 ]
--- reminder/views.py
+++ reminder/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect , get_object_or_404
 from django.views import View
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView , DeleteView
 from django.urls import reverse_lazy
 import datetime
 
 from .models import Reminder
+from .forms import ReminderForm
 
 
 
@@ -12,20 +13,19 @@ class reminder_list(View):
     template_name = 'reminder/reminder_list.html'
 
     def get(self,request):
-        reminder = Reminder.objects.all()
         today = datetime.date.today()
-        near_reminder = Reminder.objects.filter(show_date = today)
-        context = {'reminder': reminder, 'near_reminder':near_reminder}
+        near_reminder = Reminder.objects.filter(show_date = today).order_by('reminder_date')
+        context = {'near_reminder':near_reminder}
         return render (request, self.template_name, context=context)
     
     def post (self,request):
-        new_reminder = Reminder.objects.create(
-            title = request.POST.get('title'),
-            message = request.POST.get('message'),
-            reminder_date = request.POST.get('reminder_date'),
-            show_date = request.POST.get('show_date'),
-        )
-        new_reminder.save()
+        form = ReminderForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title'] 
+            message = form.cleaned_data ['message'] 
+            reminder_date = form.cleaned_data ['reminder_date'] 
+            show_date = form.cleaned_data ['show_date'] 
+            form.save()
         return redirect('/')
 
 
@@ -33,7 +33,7 @@ class reminder_detail(View):
     template_name = 'reminder/reminder_detail.html'
 
     def get(self,request,pk):
-        reminder = Reminder.objects.filter(pk=pk)
+        reminder = get_object_or_404(Reminder, pk=pk)
         context = {'reminder': reminder}
         return render (request, self.template_name, context=context)
 
@@ -45,10 +45,11 @@ class reminder_total_list(View):
         context = {'reminder': reminder}
         return render (request, self.template_name, context=context)
 
-def delete(request,pk):
-        reminder = Reminder.objects.filter(pk=pk)
-        reminder.delete()
-        return redirect ('/')
+
+class reminder_Delete(DeleteView):
+    model = Reminder
+    success_url = reverse_lazy('reminder:reminder_list')
+
 
 
 class rimender_Update(UpdateView):
