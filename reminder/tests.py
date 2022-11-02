d301587d1cf9fd1dd53c23464c80df24c67cfb61
MohammadReza0044 <you@example.com>
2022-11-02 16:44:46 +0330

    test is ok.

--- reminder/tests.py
+++ reminder/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase , SimpleTestCase
 
-# Create your tests here.
+from .forms import ReminderForm
+
+
+class TestForm(SimpleTestCase):
+
+    def test_reminder_form_valid_data(self):
+        form = ReminderForm ( data={
+            'title' : 'testcase1',
+            'message' :'this is a testcase',
+            'reminder_date' : '11/10/2022',
+            'show_date' : '11/2/2022',
+        })
+
+        self.assertTrue(form.is_valid())
+
+       
